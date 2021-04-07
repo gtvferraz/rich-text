2148eb07ff702efaaa863ba9c13ecf6f65845f5a
Gustavo Albuquerque <gtvferraz@gmail.com>
2021-04-07 20:16:24 -0300

    Criação do Menu de ferramentas

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  body, html, #__next {
     margin: 0;
     padding: 0;
+    font-size: 20px;
+  }
+
+  body {
     box-sizing: border-box;
+  }
+
+  #__next {
+    max-height: 100%;
+    background-color: #eee;
+  }
+
+  @media only screen and (max-width: 400px) {
+    #__next {
+      font-size: 5vw;
+    }
   }
 `
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
 import styled from 'styled-components'
+import Menu from '../src/components/Menu'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const Page_Container = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+
+const Main_container = styled.div`
+  width: 840px;
+  height: 50vh;
+  margin-top: 5rem;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 5px 20px -10px black;
+
+  @media only screen and (max-width: 1050px) {
+    width: calc(80vw - 2rem);
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Page_Container>
+      <Main_container>
+        <Menu />
+      </Main_container>
+    </Page_Container>
+  );
 }
